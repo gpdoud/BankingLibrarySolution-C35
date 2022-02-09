dbdce8e35403802998943489edbc1670c6aeef6b
Greg Doud <gdoud@gmail.com>
2022-02-09 11:32:20 -0500

    Add project files.

---
+++ TestBankingLibrary/Program.cs
@@ -0,0 +1,16 @@
+﻿using BankingLibraryProject;
+
+using System;
+
+namespace TestBankingLibrary {
+
+    internal class Program {
+    
+        static void Main(string[] args) {
+
+            Account account = new Account();
+            account.CalculateInterest(36);
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
