952232daa530ebaba1c40810eda3653b9ac18f78
Greg Doud <gdoud@gmail.com>
2022-02-10 13:13:45 -0500

    Final commit

--- AccountFormatLibrary/FormatAccount.cs
+++ AccountFormatLibrary/FormatAccount.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 
 namespace AccountFormatLibrary {
+
     public class FormatAccount {
 
         public static decimal CalculateInterestByMonths(int months, decimal interestRate, decimal balance) 
         {
+            if(balance < 0) {
+                throw new ArgumentException("Balance cannot be negative!");
+            }
             return balance * (interestRate / 12) * months;
         }
     }
