9f7f8ce672c15cb568ce0a8641d505863c28cf04
gpdoud <gpdoud@gmail.com>
2022-03-18 23:53:22 -0400

    final commit

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BankingLibraryProject;
+using Xunit;
+
+namespace UnitTestProject {
+    public class UnitTest1 {
+
+        Account acct;
+
+        public UnitTest1() {
+            acct = new Account();
+            acct.Balance = 100;
+        }
+        [Fact]
+        public void Test1() {
+            acct.CalculateInterest(36);
+            Assert.Equal(136, acct.Balance);
+            acct.Balance = 100;
+            acct.CalculateInterest(24);
+            Assert.Equal(124, acct.Balance);
+            acct.Balance = 100;
+            acct.CalculateInterest(12);
+            Assert.Equal(112, acct.Balance);
+        }
+    }
+}
